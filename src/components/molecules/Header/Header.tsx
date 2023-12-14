1fe8c17d0733d3d6c5411279a976d3e33dcf18b1
Wesley Rafael <wesley.santos@mblabs.com.br>
2023-12-14 09:17:48 -0300

    feat: add icons in header

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,5 +1,7 @@
-import { Flex } from '@/components/atoms'
+import { Box, Flex } from '@/components/atoms'
 import Image from 'next/image'
+import { FaRegHeart } from 'react-icons/fa'
+import { RiShoppingCartLine } from 'react-icons/ri'
 
 const Header = () => {
   return (
@@ -11,10 +13,24 @@ const Header = () => {
           width={128}
           height={50}
         />
-        <Flex className="items-center gap-[2.1875rem]">
+        <Flex className="items-center gap-[2.1875rem] flex-1">
           <p className="font-bold text-primary hover:text-primary">Home</p>
           <p className="font-bold hover:text-primary">Produtos</p>
           <p className="font-bold hover:text-primary">Favoritos</p>
+        </Flex>
+        <Flex className="gap-7 items-center">
+          <Box className="relative">
+            <FaRegHeart className="text-purple text-lg" />
+            <Box className="absolute top-[-0.9563rem] right-[-1.1938rem] bg-secondary center rounded-full w-[1.25rem] h-[1.25rem]">
+              <p className="text-white text-sm">0</p>
+            </Box>
+          </Box>
+          <Box className="relative">
+            <RiShoppingCartLine className="text-purple text-lg" />
+            <Box className="absolute top-[-0.9563rem] right-[-1.1938rem] bg-secondary center rounded-full w-[1.25rem] h-[1.25rem]">
+              <p className="text-white text-sm">0</p>
+            </Box>
+          </Box>
         </Flex>
       </Flex>
     </Flex>
